619c23264b2c535269ad5ca6518e84b05260a28d
Laurence Bradford <laurencebradford3@gmail.com>
2014-11-21 18:44:32 -0500

    added bootstrap, styled briefly.

---
+++ main.js
@@ -0,0 +1,17 @@
+Router.configure({
+   layoutTemplate: 'layout',
+   yieldTemplate: {
+      header: {
+         to: 'header'
+      },
+      footer: {
+         to: 'footer'
+      }
+   }
+});
+
+Router.map(function() {
+    this.route('home', {path: '/'});
+    this.route('about');
+    this.route('contact');
+});
